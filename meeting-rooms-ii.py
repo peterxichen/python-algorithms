c1631ba2ea54b3fe0ae77e9c92ed2aed10824d6a
peterxichen <peterxichen@outlook.com>
2022-07-02 18:48:51 -0700

    meeting rooms ii (min rooms needed)

---
+++ meeting-rooms-ii.py
@@ -0,0 +1,15 @@
+# Meeting Rooms II
+# sort by start time, put end times into min heap, top/min room frees earliest
+# iter through times append to heap, pop top room if free (< cur start time)
+# time: O(nlogn), space: O(n)
+class Solution:
+    def minMeetingRooms(self, intervals: List[List[int]]) -> int:
+        rooms = [] # minheap/priortiy queue w/ end times
+        intervals.sort(key=lambda x: x[0]) # sort by start time
+        heapq.heappush(rooms, intervals[0][1]) # add 1st meeting
+        for i in intervals[1:]: # remaining meetings
+            # top (min) room frees earliest
+            if rooms[0] <= i[0]: # room is freed up
+                heapq.heappop(rooms) # logN, remove min
+            heapq.heappush(rooms, i[1])
+        return len(rooms) # rooms = final end time of each room
